82ffaec344260b25b4136fd46aa7a03da3225079
aguilarandress <andres.aguilarmoya2001@gmail.com>
2022-02-23 15:18:38 -0600

    Initial commit

---
+++ Assets/HMDInfoManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDInfoManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Is Device active " + XRSettings.isDeviceActive);
+        Debug.Log("Device Name is:" + XRSettings.loadedDeviceName);
+
+        if (!XRSettings.isDeviceActive)
+        {
+            Debug.Log("No headset plugged");
+        }
+        else if (XRSettings.isDeviceActive && (XRSettings.loadedDeviceName == "Mock HMD"
+            || XRSettings.loadedDeviceName == "MockHMDDisplay"))
+        {
+            Debug.Log("Using Mock HMD");
+        }
+        else
+        {
+            Debug.Log("We have a headset " + XRSettings.loadedDeviceName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
